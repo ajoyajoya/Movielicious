c7e407f26092f35d3d8195ff673c578b1a39208d
Aji Prastio Wibowo <ajoyajoya@gmail.com>
2019-06-23 17:09:22 +0700

    Initial commit

---
+++ app/src/main/java/com/ajoyajoya/movielicious/Movie.java
@@ -0,0 +1,52 @@
+package com.ajoyajoya.movielicious;
+
+public class Movie {
+    private int moviePoster;
+    private String movieName;
+    private String movieRated;
+    private String movieCategory;
+    private String movieDesc;
+
+    public int getMoviePoster() {
+        return moviePoster;
+    }
+
+    public void setMoviePoster(int moviePoster) {
+        this.moviePoster = moviePoster;
+    }
+
+    public String getMovieName() {
+        return movieName;
+    }
+
+    public void setMovieName(String movieName) {
+        this.movieName = movieName;
+    }
+
+    public String getMovieRated() {
+        return movieRated;
+    }
+
+    public void setMovieRated(String movieRated) {
+        this.movieRated = movieRated;
+    }
+
+    public String getMovieCategory() {
+        return movieCategory;
+    }
+
+    public void setMovieCategory(String movieCategory) {
+        this.movieCategory = movieCategory;
+    }
+
+    public String getMovieDesc() {
+        return movieDesc;
+    }
+
+    public void setMovieDesc(String movieDesc) {
+        this.movieDesc = movieDesc;
+    }
+
+
+
+}
